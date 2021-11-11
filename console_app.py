31761f99d53a16ede8334928521bdfd0e5aa56a6
RenZorRUS <renzor.wrk@gmail.com>
2021-11-11 23:46:19 +0300

    feat: update console_app.py

--- console_app.py
+++ console_app.py
@@ -1,9 +1,11 @@
 from os import path
 from api.main_menu import main_menu
 from api.check_db import check_db
+from api.screen_clear import screen_clear
 
 BASE_DIR = path.dirname(path.abspath(__file__))
 db_path = path.join(BASE_DIR, 'db.sqlite3')
 
 check_db(db_path)
 main_menu(db_path)
+screen_clear()
